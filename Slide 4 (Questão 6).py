420158b10c0c38b6e59c5b17b1ef40cf54149b0d
FAN-Corp <162445929+FAN-Corp@users.noreply.github.com>
2024-03-06 08:35:56 -0300

    Add files via upload
    Apresentação de números maiores que 3.

---
+++ Slide 4 (Questão 6).py
@@ -0,0 +1,4 @@
+print("6-Ler um valor numérico inteiro qualquer e fazer a sua apresentação caso  o valor seja maior que três, Utilize apenas o operador lógico not para a solução deste problema")
+n1 = int(input("Escolha um número:"))
+if not(n1<=3):
+  print("Então... Seu número... Seu número é:", n1) 
